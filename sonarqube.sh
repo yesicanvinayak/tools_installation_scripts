2cdcf2153246649682b35fe3533062dac0ff140a
yesicanvinayak <102537841+yesicanvinayak@users.noreply.github.com>
2023-11-22 12:25:50 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -4,4 +4,5 @@
 # Auther: Vinayak Salunke 
 # Date: 25-7-2023
 ###########################################################################
+#!/bin/bash
 docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
