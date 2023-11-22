e1ae9b2ccac8f5514f5075a95559dec04640b93a
yesicanvinayak <102537841+yesicanvinayak@users.noreply.github.com>
2023-11-22 12:18:09 +0530

    Update Maven.sh

--- Maven.sh
+++ Maven.sh
@@ -1,3 +1,9 @@
+###########################################################################
+# Script: Maven.sh
+# Purpose: Install Maven
+# Auther: Vinayak Salunke 
+# Date: 25-7-2023
+###########################################################################
 sudo apt update -y
 sudo apt install maven -y
 mvn -version
